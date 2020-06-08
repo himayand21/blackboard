a2528eaf6b0f65ca3e7f747dafa020a271c5dc72
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-06-09 01:34:05 +0530

    feat(otp): Add six seperate input boxes for OTP

--- src/pages/welcome/VerifyOTP.js
+++ src/pages/welcome/VerifyOTP.js
@@ -1,23 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef, createRef } from 'react';
 import PropTypes from 'prop-types';
-import {useHistory} from 'react-router-dom';
-import {NavBar} from '../../components/navBar';
-import {Footer} from '../../components/footer';
-import {Loader} from '../../components/loader';
-import {WELCOME} from '../../constants';
-import {Background} from '../../components/background';
-import {useToast} from '../../components/toast';
+import { useHistory } from 'react-router-dom';
+import { NavBar } from '../../components/navBar';
+import { Footer } from '../../components/footer';
+import { Loader } from '../../components/loader';
+import { WELCOME } from '../../constants';
+import { Background } from '../../components/background';
+import { useToast } from '../../components/toast';
 
-import {resendOtpAPI} from '../../api/resendOTP';
-
+import { resendOtpAPI } from '../../api/resendOTP';
+const OTPinputs = [{ id: 'input1' }, { id: 'input2' }, { id: 'input3' }, { id: 'input4' }, { id: 'input5' }, { id: 'input6' }]
 const VerifyOTP = (props) => {
     const [email, setEmail] = useState('');
-    const [otp, setOtp] = useState('');
+    const [otp, setOtp] = useState({
+        input1: '',
+        input2: '',
+        input3: '',
+        input4: '',
+        input5: '',
+        input6: ''
+    });
 
     const history = useHistory();
     const addToast = useToast();
 
-    const {withAuthProps} = props;
+    const { withAuthProps } = props;
 
     const {
         sendOTP,
@@ -29,6 +36,7 @@ const VerifyOTP = (props) => {
         setEnterOTPVisible
     } = withAuthProps;
 
+    const inputsRef = useRef(OTPinputs.map(() => createRef()));
     useEffect(() => {
         setEnterOTPVisible(false);
     }, []);
@@ -36,7 +44,29 @@ const VerifyOTP = (props) => {
     useEffect(() => {
         if (user?.email) setEmail(user.email);
     }, [user]);
-
+    const isOneDigitNumber = (value) => !isNaN(value) && value.length === 1;
+    const handleOTPinput = (event) => {
+        const { value, dataset: { otpid, index } } = event.target;
+        if (otp[otpid] === '') {
+            setOtp({ ...otp, [otpid]: isOneDigitNumber(value) ? value : otp[otpid] });
+            isOneDigitNumber(value) && inputsRef.current[parseInt(index) + 1].focus();
+        }
+        else if (value === "") {
+            setOtp({ ...otp, [otpid]: value });
+        }
+        else {
+            const newValue = value.slice(otp[otpid].length) || value;
+            setOtp({ ...otp, [otpid]: isOneDigitNumber(newValue) ? newValue : otp[otpid] });
+            isOneDigitNumber(newValue) && inputsRef.current[parseInt(index) + 1].focus();
+        }
+    }
+    const checkBackSpace = (event) => {
+        const { value, dataset: { otpid, index,  } } = event.target;
+        if (event.key == 'Backspace', event.keyCode === 8 && !otp[otpid] ) {
+            event.preventDefault();
+            parseInt(index) > 0 && inputsRef.current[parseInt(index) - 1].focus();
+        }
+    }
     const handleVerifyOTP = () => {
         verifyOTP({
             id: user.id,
@@ -87,20 +117,28 @@ const VerifyOTP = (props) => {
                         <i className="fas fa-arrow-left" />
                     </button>
                 </NavBar>
-                {enterOTPVisible ? (
+                {1 ? (
                     <section className="login-content">
                         <article className="login-article article">
                             <div className="login-modal animate-1">
                                 <div className="login-header">Verify OTP</div>
                                 <div className="login-subheader">Please enter the 6-digit OTP sent to <span className="login-email">{email}</span>.</div>
                                 <div className="login-form">
-                                    <div className="form-row">
-                                        <div className="form-label">OTP</div>
-                                        <input
-                                            autoFocus
-                                            value={otp}
-                                            onChange={(e) => setOtp(e.target.value)}
-                                        />
+                                    <div className="form-row otp-inputs">
+                                        {
+                                            OTPinputs.map((each, index) => (
+                                                <input
+                                                    autoFocus={index == 0}
+                                                    value={otp[each.id]}
+                                                    data-otpid={each.id}
+                                                    data-index={index}
+                                                    onChange={handleOTPinput}
+                                                    ref={el => inputsRef.current[index] = el}
+                                                    key={each.id}
+                                                    onKeyDown={checkBackSpace}
+                                                />
+                                            ))
+                                        }
                                     </div>
                                     <div className="form-error-row" />
                                 </div>
@@ -122,32 +160,32 @@ const VerifyOTP = (props) => {
                         </article>
                     </section>
                 ) : (
-                    <section className="login-content">
-                        <article className="login-article article">
-                            <div className="login-modal animate-1">
-                                <div className="login-header">Verify your Email Address</div>
-                                <div className="login-subheader">{`Please confirm to request an OTP to your registered email address.`}</div>
-                                <div className="login-form">
-                                    <div className="form-row">
-                                        <div className="form-label">EMAIL</div>
-                                        <input
-                                            value={email}
-                                            readOnly={Boolean(user)}
-                                            onChange={updateEmail}
-                                        />
+                        <section className="login-content">
+                            <article className="login-article article">
+                                <div className="login-modal animate-1">
+                                    <div className="login-header">Verify your Email Address</div>
+                                    <div className="login-subheader">{`Please confirm to request an OTP to your registered email address.`}</div>
+                                    <div className="login-form">
+                                        <div className="form-row">
+                                            <div className="form-label">EMAIL</div>
+                                            <input
+                                                value={email}
+                                                readOnly={Boolean(user)}
+                                                onChange={updateEmail}
+                                            />
+                                        </div>
+                                        <div className="form-error-row" />
                                     </div>
-                                    <div className="form-error-row" />
+                                    <footer className="login-footer">
+                                        <div className="login-footer-link-wrapper" />
+                                        <button className="standard-button" onClick={requestOTP}>
+                                            {loading ? <Loader /> : 'Confirm'}
+                                        </button>
+                                    </footer>
                                 </div>
-                                <footer className="login-footer">
-                                    <div className="login-footer-link-wrapper" />
-                                    <button className="standard-button" onClick={requestOTP}>
-                                        {loading ? <Loader /> : 'Confirm'}
-                                    </button>
-                                </footer>
-                            </div>
-                        </article>
-                    </section>
-                )}
+                            </article>
+                        </section>
+                    )}
                 <Footer />
             </main>
         </div>
